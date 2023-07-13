8e4906285663f7789b6a28ed34b32a53c73dcc37
Matt Hudson <battmatt2009@gmail.com>
2023-07-13 11:00:25 -0500

    Added a new function myfun

---
+++ R/myfun.R
@@ -0,0 +1,12 @@
+#' @title My first function
+#'
+#' @param x A quantitative vector #What is the input
+#'
+#' @return A quantitative vector, squares param #what does it return?
+#' @export
+#'
+#' @examples
+#' myfun(x = 1:10)
+myfun <- function(x){
+  x^2
+}
